56dd6c40b7dff4446001930c678ca8a582203797
Ünal Çot <unalcot@hotmail.com>
2023-11-09 19:23:14 +0300

    abc

--- api/app.js
+++ api/app.js
@@ -19,6 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+  console.log("ben middlewareyim");
+  next();
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
